1d57ac7d4c9e8327f3210b9e1cb729ac07601a00
itayk93 <itayk93@gmail.com>
2025-09-20 12:33:14 +0300

    Initial Commit

---
+++ CouponManagerApp/CouponManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponManagerAppApp.swift
+//  CouponManagerApp
+//
+//  Created by itay karkason on 20/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CouponManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
